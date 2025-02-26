e9e424dc7d1e64d4bf84a9af89fcca2b8674a5bc
SAI PRANAY TADAKAMALLA <134729119+PranayTadakamalla@users.noreply.github.com>
2025-02-27 04:39:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"), // Change this to just "dist"
     emptyOutDir: true,
   },
 });
